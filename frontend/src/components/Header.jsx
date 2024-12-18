756441907a10e572fc46114ca023ade9475a0b3c
miracalagass <alagasmirac@gmail.com>
2024-12-18 22:09:01 +0100

    header update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,26 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 
 const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  //padding: 20px;
   background-color: #fff;
   @media (max-width: 768px) {
     flex-direction: row;
     width: 100%;
     padding: 4px;
-    img{
+    img {
       width: 145px;
       height: 47px;
     }
   }
-
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 20px;
-  width: 18%;
+  width: 27%;
   @media (max-width: 768px) {
     width: 45%;
   }
@@ -34,9 +32,9 @@ const NavLink = styled(Link)`
   font-weight: 500;
   font-size: 24px;
   width: 50%;
-  &:hover {
-    text-decoration: underline;
-  }
+  text-decoration: ${(props) => (props.active ? "underline" : "none")};
+  text-underline-offset: 5px; /* Çizginin yazıdan uzaklığını ayarlar */
+
   @media (max-width: 768px) {
     font-size: 12px;
     text-transform: uppercase;
@@ -44,14 +42,20 @@ const NavLink = styled(Link)`
 `;
 
 export default function Header() {
+  const location = useLocation(); // Şu anki URL yolunu kontrol etmek için
+
   return (
     <HeaderContainer>
       <Link to="/">
         <img src="/LOGO.png" alt="Kasa Logo" />
       </Link>
       <Nav>
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/about">A Propos</NavLink>
+        <NavLink to="/" active={location.pathname === "/"}>
+          Accueil
+        </NavLink>
+        <NavLink to="/about" active={location.pathname === "/about"}>
+          A Propos
+        </NavLink>
       </Nav>
     </HeaderContainer>
   );
